04454ab6858c6cd014f0fd8e88bc9699c1e52a5f
sexysuhuii <145473212+sexysuhuii@users.noreply.github.com>
2023-09-19 23:59:56 +0900

    Create today05.java
    today work

---
+++ tree/src/today05.java
@@ -0,0 +1,22 @@
+
+public class today05 {
+
+	public static void main(String[] args) {
+//		int i = 0;
+//		while (i < 10) {
+//			System.out.println(i);
+//			i++;
+//		}
+		int total = 0;
+		int i = 1;
+		while (i <= 100) {
+			total += i;
+			i++;
+		}
+		System.out.println(total);
+
+		while (true) {
+			System.out.println("hello");
+		}
+	}
+}
